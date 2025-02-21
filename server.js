9d852f5fbc070dd6d48b735f4fef75a5ed8e928a
Kokodaks <dayeonko357@gmail.com>
2025-02-21 22:12:29 +0900

    fixed : server.js for requests from android emulator (api test)

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ io.on("connection", (socket) =>{
 });
 
 //Websocket, RestfulAPI 서버 실행
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
